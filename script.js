b6174be7f52bd3c80fe236f34ff6747e5a88cada
Benson <bensonmakau2000@gmail.com>
2022-07-23 12:32:55 +0300

    display map using leaflet

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 //How To Use the GeoLocation API
 if (navigator.geolocation) {
+  //Takes as arguments, 2 callback functions
+  // (one called on success (called with a parameter called the position parameter), one called when error happens)
   navigator.geolocation.getCurrentPosition(
     function (position) {
       //   console.log(position);
@@ -22,11 +24,28 @@ if (navigator.geolocation) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
       //   console.log(latitude, longitude);
+
+      const coords = [latitude, longitude];
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const map = L.map('map').setView(coords, 13); //13 here is a zoom ratio
+
+      //string passed in the map function must be the id name of an elememnt in the html,
+      // the map will be displayed in that element
+
+      //https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your Location');
     }
   );
 }
-//Takes as arguments, 2 callback functions (one called on success (called with a parameter called the position parameter), one called when error happens)
