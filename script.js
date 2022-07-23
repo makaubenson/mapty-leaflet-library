513bb47137cb6d1b4d5a54837bfea4cbf2605ace
Benson <bensonmakau2000@gmail.com>
2022-07-23 13:02:13 +0300

    customizing marker in leaflet library

--- script.js
+++ script.js
@@ -29,20 +29,35 @@ if (navigator.geolocation) {
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
       const map = L.map('map').setView(coords, 13); //13 here is a zoom ratio
-
       //string passed in the map function must be the id name of an elememnt in the html,
       // the map will be displayed in that element
-
+      //   console.log(map);
       //https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+      //use leaflet's function on() instead of js's addEventListener
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        //Make a Marker
+        L.marker([lat, lng])
+          .addTo(map)
+          //   .bindPopup(`A pretty CSS3 popup.<br> ${lat}, ${lng}.`)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
     },
     function () {
       alert('Could not get your Location');
